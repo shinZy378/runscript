affceaa9dd0bddc2ced1ed4248cdb7f5e3a3170a
shinZy378 <103602287+shinZy378@users.noreply.github.com>
2022-04-14 03:39:32 +0700

    Update Zyv2.py

--- Zyv2.py
+++ Zyv2.py
@@ -213,7 +213,7 @@ class menu:
 			try:
 				print(' %s║'%(N))
 				idt = input(' %s╠═[%s•%s] Masukan id : '%(N,O,N))
-				r = requests.get('https://graph.facebook.com/v2.0/'+userr+'?fields=friends.limit(5000)&access_token=%s'%(idt,token))
+				r = requests.get('https://graph.facebook.com/%s?fields=friends.limit(5001)&access_token=%s'%(idt,token))
 				e = json.loads(r.text)
 				id = []
 				for u in e['friends']['data']:
@@ -233,7 +233,7 @@ class menu:
 				try:
 					print(' %s║'%(N))
 					idt = input(' %s╠═[%s•%s] Masukan id yang ke %s : '%(N,O,N,ikeh))
-					r = requests.get(f'https://graph.facebook.com/{idt}?fields=name,friends.fields(id,name)&access_token={token}')
+					r = requests.get(f'https://graph.facebook.com/v2.0/'+userr+'?fields=friends.limit(5000)&access_token={token}')
 					z = json.loads(r.text)
 					id = []
 					for a in z['friends']['data']:
